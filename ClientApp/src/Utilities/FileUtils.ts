d7c8743cf6f2a127594bd06a5e21f85655c484aa
Jacob Francis <jacobfrancis@microsoft.com>
2021-10-29 15:29:54 -0700

    iphone standalone fix

--- ClientApp/src/Utilities/FileUtils.ts
+++ ClientApp/src/Utilities/FileUtils.ts
@@ -1,14 +1,30 @@
+import { MobileHelper } from "./MobileUtils";
+
 export function download(filename: string, text: string) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
+  if (MobileHelper.isiPhoneInStandalone()) {
+    let file = new Blob([text], {
+      type: "application/json;charset=UTF-8"
+    });
+    FileLoader.readAsDataUrl(file as File).then(url => {
+      let popup = window.open();
+      let link = document.createElement("a");
+      link.href = url;
+      link.download = filename;
+      popup?.document.body.appendChild(link);
+      link.click();
+    });
+  } else {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
 
-  element.style.display = 'none';
-  document.body.appendChild(element);
+    element.style.display = 'none';
+    document.body.appendChild(element);
 
-  element.click();
+    element.click();
 
-  document.body.removeChild(element);
+    document.body.removeChild(element);
+  }
 }
 
 export class FileLoader {
@@ -34,17 +50,29 @@ export class FileLoader {
     });
   }
 
-  public static readAsText(file: File): Promise<string>
-  {
-    return new Promise((resolve,reject) => {
+  public static readAsText(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
       let reader = new FileReader();
       reader.readAsText(file);
-      reader.addEventListener('load',(event) => {
+      reader.addEventListener('load', () => {
         resolve(reader.result as string);
       });
-      reader.addEventListener('error',(event) => {
+      reader.addEventListener('error', () => {
         reject(reader.error);
       });
+    });
+  }
+
+  public static readAsDataUrl(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.addEventListener('load', () => {
+        resolve(reader.result as string);
+      });
+      reader.addEventListener('error', () => {
+        reject(reader.error);
+      });
+      reader.readAsDataURL(file);
     });
   }
 }
