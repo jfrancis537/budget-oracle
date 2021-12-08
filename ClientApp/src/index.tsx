25a8ba4c9d3cfa69750215e6307aca38c62ffe51
Jacob Francis <jacobfrancis@microsoft.com>
2021-12-07 17:20:53 -0800

    change

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -5,12 +5,19 @@ import { UserManager } from './Processing/Managers/UserManager';
 
 import './styles/global.css';
 
+function handleUncaughtError(event: ErrorEvent) {
+  if (UserManager.isLoggedIn && UserManager.username === 'francisjp') {
+    alert(event.message);
+  }
+}
+
 async function start() {
   ReactDOM.render(
     <App />,
     document.getElementById('root')
   );
   await UserManager.init();
+  window.addEventListener('error', handleUncaughtError);
 }
 
 start();
