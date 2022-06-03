bc39c6ae5d18906d5610a4da26a8e1cc2160ad47
unknown <jfrancis537@gmail.com>
2022-06-02 17:06:29 -0700

    fixed key

--- Models/LinkedAccountDetails.cs
+++ Models/LinkedAccountDetails.cs
@@ -9,13 +9,13 @@ namespace BudgetOracle.Models
     public string Name { get; set; }
     [Column("enrollment_id")]
     public string EnrollmentId { get; set; }
-    [Key, Column("user_id")]
+    [Key, Column("user_id",Order = 0)]
     public string UserId { get; set; }
     [Column("type")]
     public string Type { get; set; }
     [Column("balance_url")]
     public string BalanceUrl { get; set; }
-    [Key,Column("id")]
+    [Key,Column("id",Order = 1)]
     public string Id { get; set; }
     [Column("last_four")]
     public string LastFour { get; set; }
