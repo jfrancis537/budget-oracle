8d57e7e4774b20eeef26a3fbafbd78f3cc1ae32e
unknown <jfrancis537@gmail.com>
2023-03-04 22:47:54 -0800

    update budget oracle to net 7

--- Models/Configuration/TellerConfiguration.cs
+++ Models/Configuration/TellerConfiguration.cs
@@ -2,7 +2,6 @@
 {
   public class TellerConfiguration
   {
-    public string ApplicationId { get; set; }
     public string SSLKeyPath { get; set; }
     public string SSLCertPath { get; set; }
   }
